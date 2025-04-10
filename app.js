da14ccc31594f80f5fbce5c620eddc47b4909cef
Bruno Choutock Yukimitu <brunochoutockyk@gmail.com>
2025-04-09 22:25:58 -0300

    finalizado

--- app.js
+++ app.js
@@ -33,3 +33,46 @@ function fnIndiomaIngues() {
     document.getElementById("contato14").innerText = "Subscribe"
     document.getElementById("rodape15").innerText = "© 2025 Guto Xavier. All rights reserved."
 }
+
+function fnBaner(){
+     document.getElementById("Baner").src = "images/banner01.jpg"
+    document.getElementById("Baner").src = "images/banner02.jpg"
+    document.getElementById("Baner").src = "images/banner03.jpg"
+     document.getElementById("Baner").src = "images/banner04.jpg"
+}
+
+function fnBotao1(){
+    document.getElementById("trocaBaner").src = "images/banner01.jpg"
+}
+
+function fnBotao2(){
+    document.getElementById("trocaBaner").src = "images/banner02.jpg"
+}
+
+function fnBotao3(){
+    document.getElementById("trocaBaner").src = "images/banner03.jpg"
+}
+
+function fnBotao4(){
+    document.getElementById("trocaBaner").src = "images/banner04.jpg"
+}
+
+function fnCadastrar() {
+    let email = document.getElementById("email").value
+    alert("Seu e-mail :"+" " +email +" " + "foi cadastrado com sucesso")
+
+    //Limpar campos
+    document.getElementById("email").value = ""
+}
+
+
+function mostrarHora() {
+    const agora = new Date();
+    const hora = agora.toLocaleTimeString();
+    document.getElementById("hora").textContent = hora;
+  }
+ 
+ 
+  setInterval(mostrarHora, 1000);
+ 
+  mostrarHora();
